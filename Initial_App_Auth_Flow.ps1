e0cbef41cac60e27e9f4fd23d7b6641a2a61c407
bbzbb <46433756+bbzbb@users.noreply.github.com>
2025-06-26 23:01:54 -0400

    Update Initial_App_Auth_Flow.ps1
    Converting auth flow to a reusable function.

--- Initial_App_Auth_Flow.ps1
+++ Initial_App_Auth_Flow.ps1
@@ -4,27 +4,23 @@ $clientId     = $env:AZURE_CLIENT_ID
 $tenantId     = $env:AZURE_TENANT_ID
 $clientSecret = $env:AZURE_CLIENT_SECRET 
 
-#Build the token to use with the Graph API
+#Build the token to use with the Graph APIt
 
-$body = @{
-    client_id     = $clientId
-    scope         = "https://graph.microsoft.com/.default"
-    client_secret = $clientSecret
-    grant_type    = "client_credentials"
+function Receive-token {
+    param($clientId, $clientSecret, $tenantId)
+
+    $body = @{
+        client_id     = $clientId
+        scope         = "https://graph.microsoft.com/.default"
+        client_secret = $clientSecret
+        grant_type    = "client_credentials"
+        }
+  
+    try {
+        $tokenResponse = Invoke-RestMethod -Method POST -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -Body $body
+        return $tokenResponse.access_token
+    } catch {
+        Write-Error "Error retrieving access token: $($_.Exception.Message)"
+        return $null
+    }
 }
-
-$tokenResponse = Invoke-RestMethod -Method POST -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -Body $body
-$accessToken   = $tokenResponse.access_token
-
-#Use access token to call the Graph API
-
-$headers = @{
-    Authorization = "Bearer $accessToken"
-    "Content-Type" = "application/json"
-}
-
-#Call the Graph API to get users
-
-$response = Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/users" -Headers $headers -Method GET
-$response.value
-
